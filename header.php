4d5284f89ec383613bfc043ee05d4844a1dfa0b7
William Koller <wkoller25@gmail.com>
2019-07-22 21:06:01 -0300

    fix

--- header.php
+++ header.php
@@ -1,23 +1,37 @@
 <!doctype html>
-<html lang="pt">
+<html lang="pt-br">
 <head>
+    <!--Meta tags obrigatórias -->
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Curso PHP</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-   <header>
-       <div class="image">
-           <img src="image/dog.jpg" height="50" width="50" title="Logo" alt="Logo">
+    <div class="container-fluid">
+        <header>
+            <div class="image">
+                <img src="image/dog.jpg" height="50" width="50" title="Logo" alt="Logo">
 
-       </div>
-       <div id="menu"></div>
-   </header>
+            </div>
+            <div id="menu"></div>
+        </header>
 
-    <div id="conteudo">
+        <div id="conteudo"></div>
+    </div>
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 </html>
